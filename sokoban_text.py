87e57dfffee4b06f8856807cf96d5739b2c9a889
Hannan Khalil <khha22zo@student.ju.se>
2025-12-03 19:25:10 +0100

    make 3 text file and python file as the controller layer

---
+++ sokoban_text.py
@@ -0,0 +1,12 @@
+# final Mini-Project : Group 3
+# written By: Axel Lindh - Hannan Khalil - Emil Anderö - Viktor Tiger 
+
+# Sokoban with ASP-Based Hint System
+
+import subprocess
+import tempfile
+
+
+
+BASE_LP_FILE = "sokoban_base.lp"
+LEVEL_FILES  = ["level1.txt", "level2.txt", "level3.txt"]
