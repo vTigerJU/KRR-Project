68520efa0c3850d432c476d4d06b1ba225afaf83
Viktor Tiger <tivi22ty@student.ju.se>
2025-12-16 15:55:14 +0100

    Kinda done

--- Sokoban/controller.py
+++ Sokoban/controller.py
@@ -1,17 +1,65 @@
 import clingo
-import map_loader
+import re
 
-def on_model(m):
-    print(m)
+final_model = None
+
+
+def on_model(model):
+    global final_model
+    final_model = model.symbols(shown=True)
 
 def solve(map_facts):
-    ctl = clingo.control.Control(["--stats", "--opt-mode=optN", "-t4"])
+    ctl = clingo.control.Control(["--stats", "--opt-mode=opt", "-t4"])
     try:
         ctl.load("sokoban.lp")
-        print(map_facts)
         ctl.add("base", [], map_facts)
         ctl.ground([("base", [])])
-        print(ctl.solve(on_model=on_model))
+        result = ctl.solve(on_model=on_model)
+
+        if result.satisfiable and final_model is not None:
+           
+            return final_model
+        else:
+            return None
     except Exception as e:
         print(e)
 
+def getDirection(dir,x,y):
+    match dir:
+        case "moveUp":
+            y-=1
+        case "moveDown":
+            y+=1
+        case "moveLeft":
+            x-=1
+        case "moveRight":
+            x+=1
+        case "pushUp":
+            y-=1
+        case "pushDown":
+            y+=1
+        case "pushLeft":
+            x-=1
+        case "pushRight":
+            x+=1
+    return x,y
+
+def hint(map_facts):
+    results = solve(map_facts)
+    try:
+        move = str(results[0])
+        dirMatch = re.split(r'\(',move)
+        dir = dirMatch[1]
+        match = re.findall(r'\d+', move)
+        if len(match) >= 2:
+            first_two_digits = "".join(match[:2])
+            x = int(first_two_digits[0])
+            y = int(first_two_digits[1])
+
+        x,y = getDirection(dir,x,y)
+
+        return x, y
+    
+    except:
+        return None
+    
--- Sokoban/game.py
+++ Sokoban/game.py
@@ -16,12 +16,26 @@ map = map_loader.load_level_from_file("maps/test.txt")
 # --------------------
 # Game State
 # --------------------
-player_x, player_y = map["player"]
+player_x, player_y = 0,0
 
-crates = map["boxes"]
-walls = map["walls"]
-goals = map["goals"]
+crates = set()
+walls = set()
+goals = set()
+hints = []
+no_solution = False
+banner_text = ""
 
+def load_initial_state():
+    global crates, walls, goals, no_solution, player_x, player_y, banner_text
+    map = map_loader.load_level_from_file("maps/test.txt")
+    no_solution = False
+    player_x, player_y = map["player"]
+    crates = map["boxes"]
+    walls = map["walls"]
+    goals = map["goals"]
+    banner_text = ""
+
+load_initial_state()
 # --------------------
 # Helper functions
 # --------------------
@@ -87,16 +101,35 @@ def draw_top_bar():
     for b in buttons:
         b.update(mouse_pos)
         b.draw()
+    label = font.render(banner_text, True, c.TEXT_COLOR)
+    screen.blit(
+        label, 
+        (
+            c.SCREEN_WIDTH - label.get_width() - 10,
+            (c.TOP_BAR_HEIGHT - label.get_height()) // 2
+        )
+    )
 
-def draw_win_overlay():
+def draw_overlay(overlay_text):
     overlay = pygame.Surface((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
     overlay.set_alpha(160)
     overlay.fill((0, 0, 0))
     screen.blit(overlay, (0, 0))
 
-    text = font.render("LEVEL COMPLETED!", True, (255, 255, 255))
+    text = font.render(overlay_text, True, (255, 255, 255))
     rect = text.get_rect(center=(c.SCREEN_WIDTH//2, c.SCREEN_HEIGHT//2))
     screen.blit(text, rect)
+
+def draw_hint(hints):
+    for x,y in hints:
+        center_x = x * c.TILE_SIZE + c.TILE_SIZE // 2
+        center_y = y * c.TILE_SIZE + c.TILE_SIZE + c.TOP_BAR_HEIGHT// 2
+        pygame.draw.circle(
+            screen,
+            (255, 105, 180),
+            (center_x, center_y),
+            c.GOAL_RADIUS
+        )
 
 def in_bounds(x, y):
     return 0 <= x < c.GRID_WIDTH and 0 <= y < c.GRID_HEIGHT
@@ -135,15 +168,30 @@ def is_completed(crates, goals):
 
 def reset_game():
     print("Reset game")
+    load_initial_state()
 
-def solve():
-    print("Solve")
+
+def check():
+    global no_solution, banner_text
     map_facts = map_loader.build_asp_facts(map,(player_x,player_y),crates)
     result = controller.solve(map_facts)
-    print(result)
+    if result is None:
+        no_solution = True
+        banner_text= "no solution"
+    else:
+        banner_text = "still solvable"
+    
+def hint():
+    global no_solution
+    map_facts = map_loader.build_asp_facts(map,(player_x,player_y),crates)
+    try:
+        x, y = controller.hint(map_facts)
+        print(x,y)
+        hints.append((x,y))
+    except:
+        print("No solution")
+        no_solution = True
 
-def load_level():
-    print("Load level")
 
 buttons = []
 x = c.BUTTON_PADDING
@@ -152,10 +200,10 @@ y = (c.TOP_BAR_HEIGHT - c.BUTTON_HEIGHT) // 2
 buttons.append(Button("Reset", x, y, c.BUTTON_WIDTH, c.BUTTON_HEIGHT, reset_game,screen))
 x += c.BUTTON_WIDTH + c.BUTTON_PADDING
 
-buttons.append(Button("Solve", x, y, c.BUTTON_WIDTH, c.BUTTON_HEIGHT, solve,screen))
+buttons.append(Button("Hint", x, y, c.BUTTON_WIDTH, c.BUTTON_HEIGHT, hint,screen))
 x += c.BUTTON_WIDTH + c.BUTTON_PADDING
 
-buttons.append(Button("Load", x, y, c.BUTTON_WIDTH, c.BUTTON_HEIGHT, load_level,screen))
+buttons.append(Button("Check", x, y, c.BUTTON_WIDTH, c.BUTTON_HEIGHT, check,screen))
 
 #Game Loop
 while True:
@@ -178,7 +226,7 @@ while True:
                 try_move(-1, 0)
             elif event.key == pygame.K_RIGHT:
                 try_move(1, 0)
-
+            hints = []
             # Keep player inside grid
             player_x = max(0, min(c.GRID_WIDTH - 1, player_x))
             player_y = max(0, min(c.GRID_HEIGHT - 1, player_y))
@@ -191,8 +239,10 @@ while True:
     draw_walls(walls)
     draw_goals(goals)
     draw_crates(crates)
+    draw_hint(hints)
     draw_player(player_x, player_y)
     if is_completed(crates, goals):
-        draw_win_overlay()
+        draw_overlay("Level Complete")
+    if no_solution:
+        draw_overlay("No Solution")
     pygame.display.flip()
-
